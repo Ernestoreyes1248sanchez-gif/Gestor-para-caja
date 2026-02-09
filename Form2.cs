8b9927b1ed1315f33d8de398d4ccdd6c0be360fa
Jefferszn <jeffersondgrosales@gmail.com>
2026-02-09 09:35:00 -0600

    Hi

--- Form2.cs
+++ Form2.cs
@@ -16,5 +16,10 @@ namespace Gestor_para_caja
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
